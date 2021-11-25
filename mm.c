debfaceb4b03cefb7e9f05b446a5103e7707ee6b
Alejandro Serrano <ajserrano9310@gmail.com>
2021-11-25 09:49:08 -0700

    changes

--- mm.c
+++ mm.c
@@ -49,21 +49,24 @@
 // Given a payload pointer, get the next or previous payload pointer
 #define NEXT_BLKP(bp) ((char *)(bp) + GET_SIZE(HDRP(bp)))
 #define PREV_BLKP(bp) ((char *)(bp)-GET_SIZE((char *)(bp)-OVERHEAD))
-#define N_FREE_POINTER(bp) ((char *)(bp) + sizeof(block_header))
-#define F_SET_PTR(p, ptr)  (*(size_t *)(p) = (size_t)(ptr))
-#define F_NEXT(ptr)      (*(char **)(N_FREE_POINTER(ptr)))
 
 // Helper functions
-static void* set_allocated(void *b, size_t size);
+static void *set_allocated(void *b, size_t size);
 static void extend(size_t s);
+static void remove_block_from_list(void* allocated, void *free_block);
+static void set_new_free_block(void *free_block);
+static void *find_block(void *free_block, size_t);
+static void check_free_list();
 
 // Struct that will hold the list of pages
 typedef struct free_list
 {
-  void *next;
+  struct free_list *next;
+  struct free_list *prev;
+
 } free_list;
 
-struct free_list *head; // Head of the explicit free list
+free_list *head = NULL;
 
 void *head_free_list; // head of the list
 int current_avail_size = 0;
@@ -89,7 +92,7 @@ int mm_init(void)
   // init should just restore
   // restore the pointers
   // reset the allocator
-  head_free_list = NULL;
+  head = NULL; 
   current_avail_size = 0;
   return 0;
 }
@@ -97,43 +100,47 @@ int mm_init(void)
 void *mm_malloc(size_t size)
 {
 
+  check_free_list();
   if (size == 0)
   {
     return NULL;
   }
-  if(head_free_list == NULL)
+  if (head == NULL)
   {
     extend(size);
   }
 
-  void* current = head_free_list;
+  free_list *current = head;
   int newsize;
-  void *p; 
-
-  newsize = ALIGN(size);
+  void *p = NULL;
+  newsize = ALIGN(size + OVERHEAD);
   while (current != NULL)
   {
+    int alloc = GET_ALLOC(HDRP(current));
+    int size = GET_SIZE(HDRP(current));
     if (!GET_ALLOC(HDRP(current)) && (GET_SIZE(HDRP(current)) >= newsize))
     {
-
       int allocation = GET_ALLOC(HDRP(current));
       int ss = GET_SIZE(HDRP(current));
 
       current = set_allocated(current, newsize);
       return current;
     }
-    //current_pointer = current_pointer->next;
-    current = F_NEXT(current);
+    current = current->next;
   }
-  extend(size);
-  set_allocated(p, size);
+  extend(newsize);
+  // if we reach here current is NULL
+  p = set_allocated(p, newsize);
 
   return p;
 }
 
-static void* set_allocated(void *b, size_t size)
+static void *set_allocated(void *b, size_t size)
 {
-
+  if(b == NULL)
+  {
+    b = find_block(b, size); 
+  }
   size_t extra_size = GET_SIZE(HDRP(b)) - size;
   if (extra_size > ALIGN(1 + OVERHEAD))
   {
@@ -143,6 +150,13 @@ static void* set_allocated(void *b, size_t size)
     GET_SIZE(FTRP(NEXT_BLKP(b))) = extra_size;
   }
   GET_ALLOC(HDRP(b)) = 1;
+  GET_SIZE(FTRP(b)) = size; 
+  void *new_free_block = NEXT_BLKP(b);
+  int alloc = GET_ALLOC(HDRP(new_free_block));
+  int ss = GET_SIZE(HDRP(new_free_block));
+  check_free_list();
+  remove_block_from_list(b, new_free_block);
+
   return b;
 }
 
@@ -156,45 +170,139 @@ static void extend(size_t s)
    * of the head. 
    */
 
-  int newsize = ALIGN(s + OVERHEAD);
-  current_avail_size = PAGE_ALIGN(newsize);
-  void *new_page = mem_map(PAGE_ALIGN(newsize));
+  //int newsize = ALIGN(s + OVERHEAD); Use this inside malloc
+  current_avail_size = PAGE_ALIGN(s*4 + OVERHEAD + 16);
+  void *new_page = mem_map(current_avail_size);
   new_page += 16;
   GET_ALLOC(HDRP(new_page)) = 1; // prolog header block
   GET_SIZE(HDRP(new_page)) = 32;
   GET_ALLOC(FTRP(new_page)) = 1; // prolog footer
   GET_SIZE(FTRP(new_page)) = 32;
-  new_page += 32;
 
-  GET_SIZE(HDRP(new_page)) = PAGE_ALIGN(newsize) - 48; // sets the free space
+  new_page += OVERHEAD;
+
+  GET_SIZE(HDRP(new_page)) = current_avail_size - 48; // sets the free space
   GET_ALLOC(HDRP(new_page)) = 0;
-  GET_SIZE(FTRP(new_page)) = PAGE_ALIGN(newsize) - 48;
-  
-  GET_ALLOC(HDRP(NEXT_BLKP(new_page))) = 1;
+  GET_SIZE(FTRP(new_page)) = current_avail_size - 48;
+  int size_of_block = GET_SIZE(HDRP(new_page));
+
+  GET_ALLOC(HDRP(NEXT_BLKP(new_page))) = 1; // terminator
   GET_SIZE(HDRP(NEXT_BLKP(new_page))) = 0;
 
-  if (head_free_list == NULL)
+  if (head == NULL)
   {
-    F_SET_PTR(N_FREE_POINTER(new_page), NULL);
-    head_free_list = new_page;
+
+    head = new_page;
+    head->next = NULL;
+    head->prev = NULL;
+    check_free_list();
   }
-  
-  else{
-    void* bp = head_free_list;
-    while (bp != NULL)
-    {
-      bp = NEXT_BLKP(bp);
-    }
-    bp = new_page;
+
+  else
+  {
+    set_new_free_block(new_page);
+    check_free_list();
   }
 }
+
+static void remove_block_from_list(void* allocated, void *free_block)
+{
+
+  int allocation = GET_ALLOC(HDRP(free_block));
+  int ss = GET_SIZE(HDRP(free_block));
+  int allocation = GET_ALLOC(HDRP(allocated));
+  int ss2 = GET_SIZE(HDRP(allocated));
+  free_list *current = head;
+  if(allocated == head)
+  {
+    free_list *head_next = head->next; 
+    free_list *f = free_block; 
+    head = f;
+    head->next = head_next;
+    head->prev = NULL; 
+    check_free_list();
+    return; 
+  }
+
+  free_list *allocated_next = ((free_list*)allocated)->next;
+  free_list *allocated_prev = ((free_list*)allocated)->prev;
+  free_list *free = free_block;
+  int alloc = GET_ALLOC(HDRP(free));
+  int size = GET_SIZE(HDRP(free));
+  allocated_prev->next = free; 
+  free->prev = allocated_prev;
+  free->next = allocated_next; 
+  check_free_list();
+  return; 
+
+}
+
+static void* find_block(void *free_block, size_t size)
+{
+  free_list* current = head;
+  if(head == NULL)
+  {
+    return NULL;
+  }
+
+  while (current != NULL)
+  {
+    if (!GET_ALLOC(HDRP(current)) && (GET_SIZE(HDRP(current)) >= size))
+    {
+      int allocation = GET_ALLOC(HDRP(current));
+      int ss = GET_SIZE(HDRP(current));
+      return current;
+    }
+    current = current->next;
+  }
+  return current;
+}
+
+static void set_new_free_block(void *free_block)
+{
+  free_list *current = head;
+  while(current->next != NULL)
+  {
+    current = current->next;
+  }
+  free_list *block = free_block; 
+  current->next = block;
+  block->prev = current;
+  block->next = NULL;
+  int alloc = GET_ALLOC(HDRP(block));
+  int size = GET_SIZE(HDRP(block));
+  check_free_list(); 
+  return;
+}
+
+
 
 /*
  * mm_free - Freeing a block does nothing.
  */
 void mm_free(void *ptr)
 {
-  GET_ALLOC(ptr) = 0;
-  int size = GET_SIZE(HDRP(ptr));
-  mem_unmap(ptr, size);
+  return;
+}
+
+static void check_free_list()
+{
+  int index = 0;
+  if(head == NULL)
+  {
+    printf("List is empty\n");
+    return; 
+  }
+  free_list* current = head;
+  while(current != NULL)
+  {
+    int ss = GET_SIZE(HDRP(current));
+    printf("----%d----\n", index);
+    printf("allocation: %d\n ", GET_ALLOC(HDRP(current)));
+    printf("size: %d \n", ss);
+    printf("Pointer: %p \n", current);
+    
+    current = current->next; 
+    index++;
+  }
 }
